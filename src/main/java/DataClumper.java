aa98efc1ea8b1a9e583b4b3d900075921d72e8d3
Hu Ting Ung <tingung.hu@aurea.com>
2019-05-22 14:05:30 +0800

    Add another method invocation from shouldBeClumped
    To fullfill data clump AP which need another method invocation which exceed Data Clump minimum threshold which is 3.

--- src/main/java/DataClumper.java
+++ src/main/java/DataClumper.java
@@ -1,5 +1,10 @@
 public class DataClumper {
+
     public String shouldBeClumped(String s, float f, double d1, double d2, int i, short nigel, Object o1, Object o2) {
-        return s+f+d1+d2+i+nigel+o1+o2;
+        return anotherMethod(s, f, d1);
+    }
+
+    public String anotherMethod(String s, float f, double d1) {
+        return s + f + d1;
     }
 }
